e7cf231ccc0a5ef0e8753bb76b90ba6c601edbd4
eya <eyamahjoub1@gmail.com>
2025-11-28 03:47:31 +0100

    ajout et suppression dun etudiant

--- script.js
+++ script.js
@@ -31,3 +31,46 @@ const form = document.getElementById("formulaire");
 const studentsGrid = document.getElementById("studentsGrid");
 
 
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();//empeche la page de se recharger
+    //recuperer les donnees du formulaire
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value ;
+    const phone = document.getElementById("telephone").value ;
+    const course = document.getElementById("course").value ;
+    const age = document.getElementById("age").value ;
+    //photo
+    const photoInput = document.getElementById("photo");
+    let photoURL = "https://i.pravatar.cc/150"; //image par defaut
+    if (photoInput.files && photoInput.files[0]) {
+        photoURL = URL.createObjectURL(photoInput.files[0]);
+    }
+    //creer carte etudiant 
+    const studentCard =document.createElement("div");
+    studentCard.classList.add("student-card");
+    studentCard.innerHTML = `
+         <img src="${photoURL}" alt="Photo etudiant">
+         <h3>${name}</h3>
+         <p>${age}</p>
+         <p>${email}</p>
+         <p>${phone}</p>
+         <p>${course}</p>
+         <div class="actions">
+            <button class="delete-btn">Delete</button>
+            <button class="edit-btn">Edit</button>
+         </div> `;
+    //ajouter carte a la grille 
+    studentsGrid.appendChild(studentCard);
+    //bouton supprimer
+    studentCard.querySelector(".delete-btn").addEventListener("click",()=>{
+        studentCard.remove();
+    });
+    //bouton modifier
+    studentCard.querySelector(".edit-btn").addEventListener("click",()=>{
+        alert("fonction a implementer");
+    });
+    //fermer formulaire et reset
+    formContainer.classList.remove("show");
+    form.reset();
+
+});
